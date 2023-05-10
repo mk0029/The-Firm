4e3907ee377025b0cfebe222eabc9c99f4217982
Mj-Saini <manojsaini97285@gmail.com>
2023-05-10 15:16:33 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "./Outer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
