e644dff72d63ea2e4ce1be192af48a4f45c26403
pk-jhajhria <jhajhriapankaj13@gmail.com>
2023-05-13 14:15:34 +0530

    updates

--- src/components/JourneyHero.jsx
+++ src/components/JourneyHero.jsx
@@ -7,7 +7,8 @@ function JourneyHero() {
     <>
       <section
         id="JourneyHero"
-        className="min_vh_100 d-flex flex-column bg_gray_hero position-relative">
+        className="min_vh_100 d-flex flex-column bg_gray_hero position-relative"
+      >
         <Header />
 
         <div className=" d-flex flex-grow-1 justify-content-center align-items-center flex-column position-relative">
@@ -19,14 +20,16 @@ function JourneyHero() {
                   className=" ff_Nunito fw_bolder fs_4xxl text-black mb-0 mt-2 mt-xl-0 text_shadow z_index5 position-relative"
                   data-aos="fade-down"
                   data-aos-easing="linear"
-                  data-aos-duration="3000">
+                  data-aos-duration="3000"
+                >
                   Our Digital Journey
                 </h1>
                 <p
                   className=" ff_Lora fw-normal fs_xl clr_light_black mt-xl-3 mt-2 pt-sm-1 mb-0 pb-sm-4 pe-5 position-relative z_index5"
                   data-aos="fade-right"
                   data-aos-easing="linear"
-                  data-aos-duration="3000">
+                  data-aos-duration="3000"
+                >
                   Lorem Ipsum is simply dummy text of the printing{" "}
                   <span className="d-lg-block">
                     and typesetting industry. Lorem Ipsum has been
@@ -40,7 +43,7 @@ function JourneyHero() {
               <div className="col-xl-7 col-6 position-absolute h-100 end-0 d-none d-xl-block">
                 <div className=" d-flex justify-content-end h-100 position-relative ">
                   <img
-                    className="h-100 z_index1 position-relative shadow rounded-5 "
+                    className="journey_hero_img_custom z_index1 position-relative shadow rounded-5 "
                     src={headerImg}
                     alt="headerImg"
                   />
